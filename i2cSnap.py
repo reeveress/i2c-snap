9e4b51aa96b1e449879fc5552da00ef5da019755
cam-astro <laptop@cam>
2016-12-15 18:34:45 +0000

    Write top byte of clock scale. Enable core when done
    Previous behaviour was to only write bottom byte,
    implying MSB of scale is 0xff (reset value).
    
    Also misc formatting changes.

--- i2cSnap.py
+++ i2cSnap.py
@@ -17,18 +17,26 @@ class I2C:
 		self.controller_name = controller_name
                 self.enable_core()
 
-
-
 	def clockSpeed(self, desiredSpeed, inputSpeed = 100):
 		preScale = (inputSpeed/(5*desiredSpeed))-1
 		#Clear EN bit in the control register before writing to prescale register
-		self.fpga.write_int(self.controller_name, 0x00, offset = controlReg)
+		self.disable_core()
 		#Write the preScale factor to the Prescale Register's low bit 
-		self.fpga.write_int(self.controller_name, preScale, offset = PRERlo,blindwrite=True)
+		self.fpga.write_int(self.controller_name, (preScale >> 0) & 0xff, offset = PRERlo, blindwrite=True)
+		#Write the preScale factor to the Prescale Register's high bit 
+		self.fpga.write_int(self.controller_name, (preScale >> 8) & 0xff, offset = PRERhi, blindwrite=True)
+		#Re-enable core
+		self.enable_core()
 
 	def getStatus(self):
 		status = self.fpga.read_int('i2c_ant1', offset = statusReg)
-		statusDict = {"ACK":{"desc":'Acknowledge from Slave',"val" : (status >> 7) &1}, "BUSY" : {"val" : (status >> 6)&1 , "desc" : 'Busy i2c bus'}, 'ARB' : {"val": (status >> 5)&1, "desc":'Lost Arbitration'}, "TIP" : {"val":(status>>1)&1,"desc":'Transfer in Progress'}, "INT": {"desc" : 'Interrupt Pending', "val" : status&1}}
+		statusDict = {
+			"ACK"  : {"val" : (status >> 7) & 1, "desc" :'Acknowledge from Slave',},
+			"BUSY" : {"val" : (status >> 6) & 1, "desc" : 'Busy i2c bus'},
+			"ARB"  : {"val" : (status >> 5) & 1, "desc" :'Lost Arbitration'},
+			"TIP"  : {"val" : (status >> 1) & 1, "desc" :'Transfer in Progress'},
+			"INT"  : {"val" : (status >> 0) & 1, "desc" : 'Interrupt Pending'},
+			}
 		return statusDict
 	
 	def _strobeStartBit(self):
@@ -43,31 +51,26 @@ class I2C:
 		Toggle write bit to indicate that writing to slave is about to take place 
 		"""
 		self.fpga.write_int(self.controller_name, 0x10 , offset = commandReg,blindwrite=True)
+
 	def _strobeReadBit(self):
 		"""
 		Toggle read bit to prepare slave for reading
 		"""
 		self.fpga.write_int(self.controller_name, 0x20 , offset = commandReg,blindwrite=True)
-		
-		
 
 	def _strobeStopBit(self):
 		"""
 		Toggle stop bit to indicate end of transmission
 		"""
 		self.fpga.write_int(self.controller_name, 0x40 , offset = commandReg,blindwrite=True)
-		
 
 	def writeSlave(self,addr,data):
 		#write address + write bit to Transmit Register, shifting to create bottom bit
 		#to specify read/write operation
 		self.fpga.write_int(self.controller_name, (addr<<1) , offset = transmitReg,blindwrite=True)
 		
-		
 		#set STA bit
 		self._strobeStartBit()	
-		
-		
 		
 		#set WR bit
 		self._strobeWriteBit()
@@ -76,37 +79,26 @@ class I2C:
 		while (self.getStatus()["TIP"]["val"]):
 			time.sleep(.05)
 
-
-
 		#read RxACK bit from status register, should be 0
 		while self.getStatus()["ACK"]["val"]:
 			time.sleep(.05)
 		
-		
 		#write data to Transmit registrer
 		self.fpga.write_int(self.controller_name, data, offset = transmitReg,blindwrite=True)
-		
-
 
 		#set STO bit
 		self._strobeStopBit()
 
-
-
 		#set WR bit
 		self._strobeWriteBit()
-		
 		
 		##WAIT FOR INTERRUPT OR TIP FLAG TO NEGATE, 0 when complete
 		while (self.getStatus()["TIP"]["val"]):
 			time.sleep(.05)
 
-
-
 		#read RxACK bit from status register, should be 0
 		while self.getStatus()["ACK"]["val"]:
 			time.sleep(.05)
-
 
 	def readSlave(self,slaveAddr, memLoc=0):
 		#write address+write bit to transmit register
@@ -122,27 +114,20 @@ class I2C:
 		while (self.getStatus()["TIP"]["val"]):
 			time.sleep(.05)
 
-
-
 		#read RxACK bit from status register, should be 0
 		while self.getStatus()["ACK"]["val"]:
 			time.sleep(.05)
-
 
 		#write memory address to read from to Transmit register
 		self.fpga.write_int(self.controller_name, memLoc , offset = transmitReg,blindwrite=True)
 
-		
 		##WAIT FOR INTERRUPT OR TIP FLAG TO NEGATE, 0 when complete
 		while (self.getStatus()["TIP"]["val"]):
 			time.sleep(.05)
 
-
-
 		#read RxACK bit from status register, should be 0
 		while self.getStatus()["ACK"]["val"]:
 			time.sleep(.05)
-
 
 		#write address + read bit  to transmit register
 		self.fpga.write_int(self.controller_name, (slaveAddr<<1)|0x01 , offset = transmitReg,blindwrite=True)
@@ -184,4 +169,3 @@ class I2C:
 		"""
 		I2C_ENABLE_OFFSET = 7
 		self.fpga.write_int(self.controller_name, 0<<I2C_ENABLE_OFFSET, offset=controlReg)
-	

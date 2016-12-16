2084656560dc74357dd9c89ef7cde8d187d02318
Jack <jack@cambridge>
2016-12-16 15:11:23 +0000

    Added simple script to flash LEDs

---
+++ test_led.py
@@ -0,0 +1,23 @@
+import corr
+import i2cSnap
+import time
+
+HOST = '10.1.0.23'
+LED0_ADDR = 0x21
+LED1_ADDR = 0x20
+
+r = corr.katcp_wrapper.FpgaClient(HOST)
+time.sleep(0.1)
+
+i2c = i2cSnap.I2C(r, 'i2c_ant1')
+i2c.clockSpeed(200)
+
+i=0
+while(True):
+    i2c.writeSlave(LED0_ADDR, i%256)
+    i2c.writeSlave(LED1_ADDR, i%256)
+    i += 1
+    time.sleep(0.05)
+
+
+

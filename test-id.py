287a214d76a7513e5be565f2dfdf3b85161cf858
Tian Huang <ian.h.0628@gmail.com>
2017-03-07 10:34:07 +0000

    Modify id chip and temp sensor test scripts to let rpi operate the I2C bus

--- test-id.py
+++ test-id.py
@@ -1,15 +1,23 @@
-import smbus
+import corr
+import i2cSnap
+import time
 
-bus = smbus.SMBus(1)
+HOST = '10.1.0.23'
 addr = 0x50
 
-bus.write_byte(addr,0)
-print ("%02x"%bus.read_byte(addr))
-print ("%02x"%bus.read_byte(addr))
-print ("%02x"%bus.read_byte(addr))
-print ("%02x"%bus.read_byte(addr))
-print ("%02x"%bus.read_byte(addr))
-print ("%02x"%bus.read_byte(addr))
-print ("%02x"%bus.read_byte(addr))
-print ("%02x"%bus.read_byte(addr))
+r = corr.katcp_wrapper.FpgaClient(HOST)
+time.sleep(0.1)
+
+i2c = i2cSnap.I2C(r, 'i2c_ant1')
+i2c.clockSpeed(200)
+
+i2c.writeSlave(addr, 0)
+print i2c.readSlave(addr)
+print i2c.readSlave(addr)
+print i2c.readSlave(addr)
+print i2c.readSlave(addr)
+print i2c.readSlave(addr)
+print i2c.readSlave(addr)
+print i2c.readSlave(addr)
+print i2c.readSlave(addr)
 
